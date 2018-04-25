16403c7ab7bde73746cd130844abc08a4c67cbe3
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-25 17:41:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -40,7 +40,7 @@ class App < Sinatra::Base
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
     if @operator == "add"
-      @num1
+      params[:number1].to_i
     end
   end
 
