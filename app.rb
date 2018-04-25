8dd536f52e8696390b2f6dd1c49c39bceb71de51
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-25 17:13:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,8 +17,8 @@ class App < Sinatra::Base
     "#{@output}"
   end
 
-  get "/say/:word1/:word2/:word3/:word4/:word5" do 
-    @pharse = "##{params[:word1]} #{[:word1]} #{[:word1]} #{[:word1]} #{[:word1]}" 
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @pharse = "##{params[:word1]} #{[:word1]} #{[:word1]} #{[:word1]} #{[:word1]}"
     "#{@phrase}"
   end
 
